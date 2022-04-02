fc2d4375a4dc10fac56ebef17fbd2e9b1ccea1d2
Jorge Sucuzhañay <69119656+jorge-sucuzhanay@users.noreply.github.com>
2022-04-01 21:17:27 -0500

    detalle rutina

--- bemuscle/bemuscle-admin/src/app.js
+++ bemuscle/bemuscle-admin/src/app.js
@@ -62,8 +62,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./rutas/index.ruta'))
 app.use(require('./rutas/login.ruta'))
 app.use('/ejercicio',require('./rutas/ejercicio.ruta'))
-app.use('/clasificacion',require('./rutas/clasificacion.ruta'))
-app.use('/subclasificacion',require('./rutas/subclasificacion.ruta'))
+app.use('/detalleRutina',require('./rutas/detalleRutina.ruta'))
+app.use('/subdetalleRutina',require('./rutas/subdetalleRutina.ruta'))
 
 
 module.exports = app;
---
+++ bemuscle/bemuscle-admin/src/controladores/detalleRutina.controlador.js
@@ -0,0 +1,59 @@
+const detalleRutinactl = {}
+
+const orm = require('../configuracion_base_datos/base.orm')
+
+const sql = require('../configuracion_base_datos/base.sql')
+
+detalleRutinactl.mostrar = (req, res) => {
+    res.render('detalleRutina/detalleRutinaAgregar');
+}
+
+detalleRutinactl.mandar = async (req, res) => {
+
+    const id = req.user.idUsuarios
+    const { consejos} = req.body
+    const nuevoDetalleRutina = {
+        consejos,
+        usuarioIdUsuarios: id
+    }
+    await orm.detalleRutina.create(nuevoDetalleRutina)
+    req.flash('success', 'Se guardó correctamente')
+
+    res.redirect('/detalleRutina/lista/' + id);
+}
+
+detalleRutinactl.listar = async (req, res) => {
+    const id = req.user.idUsuarios
+    const lista = await sql.query('select * from detalle_rutinas ')
+    res.render('detalleRutina/detalleRutinaLista', { lista })
+
+}
+
+detalleRutinactl.eliminar = async (req, res) => {
+    const id = req.params.id
+    const ids = req.user.idUsuarios
+    await orm.detalleRutina.destroy({ where: { detalle_rutina_id: id } })
+    req.flash('success', 'Se eliminó correctamente')
+    res.redirect('/detalleRutina/lista/' + ids);
+
+}
+
+detalleRutinactl.traer = async (req, res) => {
+    const id = req.params.id
+    const lista = await sql.query('select * from detalle_rutinas where detalle_rutina_id = ?', [id])
+    res.render('detalleRutina/detalleRutinaEditar', { lista })
+}
+
+detalleRutinactl.editar = async (req, res) => {
+    const ids = req.user.idUsuarios
+    const id = req.params.id
+    const { consejos} = req.body
+    const nuevoDetalleRutina = {
+        consejos,
+    }
+await sql.query('update detalle_rutinas set ? where detalle_rutina_id = ?', [nuevoDetalleRutina,id])
+    req.flash('success', 'Se editó correctamente')
+    res.redirect('/detalleRutina/lista/' + ids);
+}
+
+module.exports = detalleRutinactl
