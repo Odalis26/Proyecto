5655d6c98a85977488974904589c6c5ca5d42d68
Abigail <laq.campo@yavirac.edu.ec>
2022-04-14 15:15:40 -0500

    Historial Cliente

--- bemuscle/bemuscle-admin/src/rutas/historialCliente.ruta.js
+++ bemuscle/bemuscle-admin/src/rutas/historialCliente.ruta.js
@@ -3,11 +3,11 @@ const rutas = express.Router()
 
 const {mostrar,  mandar, listar, eliminar, traer, editar}= require('../controladores/historialcliente.controlador')
 const {isLoggedIn} = require('../lib/auth')
-rutas.get('/Agregar/:id', isLoggedIn, mostrar)
-rutas.post('/Agregar/:id', isLoggedIn, mandar)
+rutas.get('/Agregar/', isLoggedIn, mostrar)
+rutas.post('/Agregar/', isLoggedIn, mandar)
 rutas.get('/Lista/:id', isLoggedIn, listar)
 rutas.get('/Eliminar/:id', isLoggedIn, eliminar)
 rutas.get('/Editar/:id', isLoggedIn, traer)
 rutas.post('/Editar/:id', isLoggedIn, editar)
 
-module.exports = rutas  
+module.exports = rutas
