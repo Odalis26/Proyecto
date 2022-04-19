7e8c1120cc6db9c81e788483466e6b307acced09
jsmcervantes <jsm.cervantes@yavirac.edu.ec>
2022-04-19 17:08:28 -0500

    master

--- bemuscle/bemuscle-admin/src/controladores/index.controlador.js
+++ bemuscle/bemuscle-admin/src/controladores/index.controlador.js
@@ -2,8 +2,10 @@ const indexctl ={}
 const orm = require('../configuracion_base_datos/base.orm')
 const sql = require('../configuracion_base_datos/base.sql')
 
+
 indexctl.mostrar= async(req, res)=>{
     res.render('index')
 }
+
 
 module.exports =indexctl
