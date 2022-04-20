77696d07fd6499c30120fd3053f283b489ae5537
Alexby32 <alexbenavides240@gmail.com>
2022-04-20 10:47:04 -0500

    Proyecto-Completo

--- bemuscle/bemuscle-admin/src/controladores/rutina.controlador.js
+++ bemuscle/bemuscle-admin/src/controladores/rutina.controlador.js
@@ -9,26 +9,25 @@ rutinactl.mostrar = (req, res) => {
 }
 
 rutinactl.mandar = async (req, res) => {
-    const ids =req.params.id
+
     const id = req.user.idUsuarios
-    const { video_rutina, tiempo_rutina, descripcion, progreso} = req.body
+    const { video_rutina, tiempo_rutina, descripcion, progreso } = req.body
     const nuevaRutina = {
-        video_rutina, 
-        tiempo_rutina, 
-        descripcion, 
+        video_rutina,
+        tiempo_rutina,
+        descripcion,
         progreso,
-        usuarioIdUsuarios: id,
-        ejercicioEjercicioId : ids
+        usuarioIdUsuarios: id
     }
     await orm.rutina.create(nuevaRutina)
     req.flash('success', 'Se guardó correctamente')
 
-    res.redirect('/rutina/lista/' + ids);
+    res.redirect('/rutina/lista/' + id);
 }
 
 rutinactl.listar = async (req, res) => {
-    const id = req.params.id
-    const lista = await sql.query('select * from rutinas  where ejercicioEjercicioId=?', [id])
+    const id = req.user.idUsuarios
+    const lista = await sql.query('select * from rutinas ')
     res.render('rutina/rutinaLista', { lista })
 
 }
@@ -51,14 +50,14 @@ rutinactl.traer = async (req, res) => {
 rutinactl.editar = async (req, res) => {
     const ids = req.user.idUsuarios
     const id = req.params.id
-    const { video_rutina, tiempo_rutina, descripcion, progreso} = req.body
+    const { video_rutina, tiempo_rutina, descripcion, progreso } = req.body
     const nuevaRutina = {
-        video_rutina, 
-        tiempo_rutina, 
-        descripcion, 
+        video_rutina,
+        tiempo_rutina,
+        descripcion,
         progreso,
     }
-await sql.query('update rutinas set ? where rutina_id = ?', [nuevaRutina,id])
+    await sql.query('update rutinas set ? where rutina_id = ?', [nuevaRutina, id])
     req.flash('success', 'Se editó correctamente')
     res.redirect('/rutina/lista/' + ids);
 }
